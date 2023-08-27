b8398b03b465db7748a846c821458a7e9435774e
unknown <mohanasudhakar2314@gmail.com>
2023-08-27 23:55:01 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoDayfour4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDayfour4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDayfour4Application.class, args);
+	}
+
+}
